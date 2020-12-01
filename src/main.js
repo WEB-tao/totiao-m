f17a6a946944b65734d90e69b84d18331d15b7ef
马松涛 <songtao_1996@163.com>
2020-12-01 23:55:43 +0800

    項目初始化

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式
+import './styles/index.less'
+
 Vue.config.productionTip = false
 
+// 创建vue 根实例 将router ` store 配置
+// 把APP 根组件渲染到 #app 节点
 new Vue({
   router,
   store,
